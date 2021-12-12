d4209e08405bbbfdfba96b72f2a815d5de881885
Esoteric918 <3411@holbertonschool.com>
2021-12-12 12:08:38 -0600

    pep8

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,6 +2,7 @@
 """FLASK"""
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
@@ -10,15 +11,17 @@ def hello():
     """hello HBNB"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnd', strict_slashes=False)
 def hbnb_only():
     """HBNB"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
-    """"""
     return 'C {}'.format(text.replace("_", " "))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
