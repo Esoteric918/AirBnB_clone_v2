adb0ba5fc3c1616d5258d395e2bbfd4b2e9e86ad
Esoteric918 <3411@holbertonschool.com>
2021-12-12 12:00:19 -0600

    pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,12 @@ def hello():
     """hello HBNB"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnd', strict_slashes=False)
 def hbnb_route():
     """HBNB"""
     return 'HBNB'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
