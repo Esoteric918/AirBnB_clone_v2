ffdc39cd71dabda7cdea5655861d98606ff476ea
Esoteric918 <3411@holbertonschool.com>
2021-12-12 11:55:47 -0600

    pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+""" Start FLASK web app"""
 from flask import Flask
+
 
 app = Flask(__name__)
 
@@ -7,6 +9,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
