989757ebf1a621c74b8f41818dc10495818a37b4
Esoteric918 <3411@holbertonschool.com>
2021-12-12 11:09:37 -0600

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from models import storage
+from flask import Flask, app, render_template
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """states list"""
+    res = []
+    storage.reload()
+
+    for v in storage.all("States").values():
+        res.append([v.id, v.name])
+    return render_template("7-states_list.html", states=res)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """teardown"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
