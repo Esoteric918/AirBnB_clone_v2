0148ea82a11e2374116aebda4042b9de8eec99bf
Esoteric918 <3411@holbertonschool.com>
2021-12-13 17:20:36 -0600

    fix return

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -3,34 +3,35 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map
+app.url_map.strict_slashes = False
 
-@app.route('/', strict_slashes=False)
+
+@app.route('/')
 def hello():
     """hello HBNB"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb_only():
     """HBNB"""
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def c_route(text):
     """c_route"""
     return 'C {}' .format(text.replace("_", " "))
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python')
+@app.route('/python/<text>')
 def python_route(text="is cool"):
     """python_route"""
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<int>:n', strict_slashes=False)
+@app.route('/number/<int>:n')
 def number(n):
     if isinstance(n, int):
         return "{} is a number".format(int(n))
