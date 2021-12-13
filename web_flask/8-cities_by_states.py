20b8e49873330f308a65b024ae17b6ce396d5a2a
Esoteric918 <3411@holbertonschool.com>
2021-12-13 12:51:55 -0600

    task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""flask app for states"""
+
+
+from flask import Flask, render_template
+from flask.scaffold import F
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route("/cities_by_states")
+def cities_by_states():
+    return render_template('8-cities_by_states.html',
+                            states=storage.all(State))
+
+
+@app.teardown_appcontext
+def teardown(context):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
