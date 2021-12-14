02066740c03b378abfd79e77712bccbd1a8edbfc
Esoteric918 <3411@holbertonschool.com>
2021-12-14 13:51:24 -0600

    task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""flask app for states"""
+
+
+from flask import Flask, render_template
+from flask.scaffold import F
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/cities_by_states")
+def states(id=None):
+    storage.reload()
+    states = storage.all(State)
+
+    if id:
+        states = states.get('State.{}'.format(id))
+    return render_template('9-states.html', State.states)
+
+
+@app.teardown_appcontext
+def teardown(context):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
