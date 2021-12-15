b3f6928e39fcbe81847319f5d9047412ab71d904
Esoteric918 <3411@holbertonschool.com>
2021-12-15 13:54:41 -0600

    fix State

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,7 +13,7 @@ app.url_map.strict_slashes = False
 @app.route("/cities_by_states")
 def cities_by_states():
     return render_template('8-cities_by_states.html',
-                           states=storage.all(State))
+                           States=storage.all(State))
 
 
 @app.teardown_appcontext
